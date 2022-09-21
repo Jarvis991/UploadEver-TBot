ca457674f33308081a752476c62d39b0b12c1d17
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-21 21:53:36 +0530

    Update clone.py

--- bot/plugins/clone.py
+++ bot/plugins/clone.py
@@ -1,5 +1,6 @@
 #Copyright 2022-present, Author: 5MysterySD
 
+from re import findall
 from requests import get as rget
 from config import LOGGER, USERS_API
 from bot.client import Client
@@ -7,7 +8,7 @@ from pyrogram import filters, enums
 from pyrogram.types import Message
 
 @Client.on_message(filters.regex(r'^https?://uploadever\.in\S+') & filters.private)
-async def clone_handler(c: Client, m: Message):
+async def _cloneUploadEverLinks(c: Client, m: Message):
     ''' Clone URL from UploadEver.in Server API:
 
     :param url: The UploadEver.in URL you want to Clone
@@ -15,13 +16,13 @@ async def clone_handler(c: Client, m: Message):
 
     OUTPUT:
     {
-        "status": 200,
-        "result": {
-            "url": "https://uploadever.in/r9o25tsq86ru",
-            "filecode": "r9o25tsq86ru"
-        },
-        "msg": "OK",
-        "server_time": "2022-03-09 10:49:48"
+      "status": 200,
+      "result": {
+        "url": "https://uploadever.in/r9o25tsq86ru",
+        "filecode": "r9o25tsq86ru"
+      },
+      "msg": "OK",
+      "server_time": "2022-03-09 10:49:48"
     }
     '''
 
@@ -37,3 +38,41 @@ async def clone_handler(c: Client, m: Message):
         else: text_ = jdata['msg']
 
     await m.reply_text(text=text_, parse_mode=enums.ParseMode.HTML, quote=True)
+
+
+@Client.on_message(filters.command("bulklinks") & filters.private)
+async def _bulkCloneLinks(c: Client, m: Message):
+    ''' Bulk Clone URL from UploadEver.in Server API:
+
+    :param url: The UploadEver.in URL you want to Clone
+    :param token: Your Own API token of UploadEver.in
+    '''
+
+    rpyMSG = m.reply_to_message
+    if not rpyMSG: 
+        await m.reply_text(text="🖇 <b><i>Give a UploadEver.in Link to Clone or Reply to Any Message Containing UploadEver.in Links !!</i></b>", parse_mode=enums.ParseMode.HTML, quote=True)
+        return
+    _txtLinks = rpyMSG.caption if rpyMSG.photo else rpyMSG.text
+    _retxt = findall(r'https?://uploadever\.in\S+', _txtLinks)
+    
+    LOGGER.info(f"[BULK] Clone Links : {len(_retxt)}")
+    Token = USERS_API.get(m.chat.id, None)
+    if Token is None:
+        await m.reply_text(text="<b>😬 I see, you have not Login, Do <i>/login</i> to Use this Command. </b>", parse_mode=enums.ParseMode.HTML, quote=True)
+
+    for no, link in enumerate(_retxt, 1):
+        filecode = (link.split("/"))[-1]
+        resp = rget(f"https://uploadever.in/api/file/clone?file_code={filecode}&key={Token}")
+        jdata = resp.json()
+        if jdata['status'] == 200:
+            _txtLinks = _txtLinks.replace(link, jdata['result']['url'])
+        else:
+            await m.reply_text(f"⛔️ <b>ERROR: (Link No.: {no})</b> <code>{jdata['msg']}</code>", quote=True)
+            _txtLinks = _txtLinks.replace(link, "")
+    #if rpyMSG.photo:
+    #    await m.reply_photo(photo=??, caption=_txtLinks, quote=True)
+    await m.reply_text(_txtLinks, quote=True)
+
+
+
+
