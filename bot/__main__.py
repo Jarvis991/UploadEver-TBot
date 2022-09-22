68d88c377d4ed2a5b9a9acff5f91a0a7f8f9a78c
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-22 21:53:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,7 @@
-import asyncio
 from bot import bot
-from bot.core.db.db_func import db
 
 if __name__ == "__main__":
-    asyncio.run(db.db_load())
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    #asyncio.run(db.db_load())
+    #loop = asyncio.new_event_loop()
+    #asyncio.set_event_loop(loop)
     bot.run()
