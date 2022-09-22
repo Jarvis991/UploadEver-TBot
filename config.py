ef7d3314677e2867453fbac98cf3b5b01298448e
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-22 22:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 #Copyright 2022-present, Author: 5MysterySD
 
-import os
+from os import environ as env
 import logging
+from logging import StreamHandler, basicConfig, INFO
 from time import time
 from logging.handlers import RotatingFileHandler
 
 # Logging >>>
-logging.basicConfig(
-    level=logging.INFO,
+basicConfig(
+    level=INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
     datefmt="%d-%b-%y %I:%M:%S %p",
     handlers=[
         RotatingFileHandler(
             "log.txt", maxBytes=50000000, backupCount=10
         ),
-        logging.StreamHandler(),
+        StreamHandler(),
     ],
 )
 
@@ -22,15 +23,14 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 # Initial >>>
-USERS_API = {}
 BOT_UPTIME = time()
 
 # Invoke Data >>>
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DIRECTORY = os.environ.get("DIRECTORY", "downloads/")
-    ADMINS = list(set(int(x) for x in os.environ.get("ADMINS", "0").split()))
-    MONGODB_URI = os.environ.get("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    API_ID = int(env.get("API_ID", ""))
+    API_HASH = env.get("API_HASH", "")
+    BOT_TOKEN = env.get("BOT_TOKEN", "")
+    DIRECTORY = env.get("DIRECTORY", "downloads/")
+    ADMINS = list(set(int(x) for x in env.get("ADMINS", "0").split()))
+    MONGODB_URI = env.get("DATABASE_URL", "")
+    LOG_CHANNEL = int(env.get("LOG_CHANNEL", "-100"))
